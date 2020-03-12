24b561f4d9c68692b5f0c45dbc36ca5cdd257e39
mdzoubir <57962405+mdzoubir@users.noreply.github.com>
2020-03-12 14:35:48 +0100

    Add files via upload

---
+++ workshop/Enoncée 1/js/script.js
@@ -0,0 +1,26 @@
+var favoriteBooks = ["red", "black", "white"];
+
+function addFavoriteBook(bookName) {
+
+    var book = favoriteBooks.includes("Great");
+    // console.log(book);
+    if (book !== true) {
+        favoriteBooks.push("Great")
+    } else {
+        console.log("it's aready exist");
+
+    }
+    console.log(favoriteBooks);
+
+}
+addFavoriteBook();
+
+function printFavoriteBooks() {
+    console.log("livers favoris : " + favoriteBooks[0])
+    for (const element of favoriteBooks) {
+        console.log(element);
+    }
+}
+
+
+printFavoriteBooks();
