9fb229c42fcab836137491518611d059576252e0
mdzoubir <57962405+mdzoubir@users.noreply.github.com>
2020-03-12 16:21:27 +0100

    mise a jour

--- workshop/Enoncée 1/js/script.js
+++ workshop/Enoncée 1/js/script.js
@@ -3,7 +3,7 @@ var favoriteBooks = ["red", "black", "white"];
 function addFavoriteBook(bookName) {
 
     var book = favoriteBooks.includes("Great");
-    // console.log(book);
+    console.log(book);
     if (book !== true) {
         favoriteBooks.push("Great")
     } else {
@@ -16,9 +16,14 @@ function addFavoriteBook(bookName) {
 addFavoriteBook();
 
 function printFavoriteBooks() {
+    var number = favoriteBooks.length;
+
     console.log("livers favoris : " + favoriteBooks[0])
     for (const element of favoriteBooks) {
-        console.log(element);
+        console.log(favoriteBooks.length + " " + element);
+    }
+    for (let y = 0; y < favoriteBooks.length; y++) {
+        console.log(y + " " + favoriteBooks[y])
     }
 }
 
