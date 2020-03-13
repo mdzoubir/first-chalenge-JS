e335debbb83a52c9cfd482b6ed432f0a644b48ca
mdzoubir <57962405+mdzoubir@users.noreply.github.com>
2020-03-13 14:27:46 +0100

    ajouter un seletion de la premier partie ennoncee 2

--- workshop/Enoncée 1/js/script.js
+++ workshop/Enoncée 1/js/script.js
@@ -1,31 +1,65 @@
 var favoriteBooks = ["red", "black", "white"];
 
-function addFavoriteBook(bookName) {
+// function addFavoriteBook(bookName) {
 
-    var book = favoriteBooks.includes("Great");
-    console.log(book);
-    if (book !== true) {
-        favoriteBooks.push("Great")
-    } else {
-        console.log("it's aready exist");
+//     var book = favoriteBooks.includes("Great");
+//     console.log(book);
+//     if (book !== true) {
+//         favoriteBooks.push("Great")
+//     } else {
+//         console.log("it's aready exist");
 
+//     }
+//     console.log(favoriteBooks);
+
+// }
+// addFavoriteBook();
+
+// function printFavoriteBooks() {
+//     var number = favoriteBooks.length;
+
+//     console.log("livers favoris : " + number);
+//     for (const element of favoriteBooks) {
+//         console.log(element);
+//     }
+//     // for (let y = 0; y < favoriteBooks.length; y++) {
+//     //     console.log(y + " " + favoriteBooks[y])
+//     // }
+//     const user = new String("red");
+//     console.log(user.toString());
+
+// }
+// printFavoriteBooks();
+
+
+class Bookshelf {
+    constructor() {
+        this.addFavoriteBook();
+        this.printFavoriteBooks();
     }
-    console.log(favoriteBooks);
+    addFavoriteBook(bookName) {
+        var book = favoriteBooks.includes("Great");
+        console.log(book);
+        if (book !== true) {
+            favoriteBooks.push("Great")
+        } else {
+            console.log("it's aready exist");
 
+        }
+        console.log(favoriteBooks);
+    }
+    printFavoriteBooks() {
+        var number = favoriteBooks.length;
+
+        console.log("livers favoris : " + number);
+        for (const element of favoriteBooks) {
+            console.log(element);
+        }
+        // for (let y = 0; y < favoriteBooks.length; y++) {
+        //     console.log(y + " " + favoriteBooks[y])
+        // }
+        const user = new String("red");
+        console.log(user.toString());
+    }
 }
-addFavoriteBook();
-
-function printFavoriteBooks() {
-    var number = favoriteBooks.length;
-
-    console.log("livers favoris : " + favoriteBooks[0])
-    for (const element of favoriteBooks) {
-        console.log(favoriteBooks.length + " " + element);
-    }
-    for (let y = 0; y < favoriteBooks.length; y++) {
-        console.log(y + " " + favoriteBooks[y])
-    }
-}
-
-
-printFavoriteBooks();
+window.onload = new Bookshelf();
